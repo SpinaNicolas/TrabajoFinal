8db87b0a61ac4320e159235c9ba55aec47de93e0
Spina Nicolas <spinanicolas11@gmail.com>
2025-11-03 14:49:13 -0300

    metodos listar,eliminar,agregar

--- src/Clases/Inventario.java
+++ src/Clases/Inventario.java
@@ -1,6 +1,7 @@
 package Clases;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Inventario <T extends Vehiculo>{
@@ -17,5 +18,42 @@ public class Inventario <T extends Vehiculo>{
     public void setLista(List<T> lista) {
         this.lista = lista;
     }
+
+    public void eliminarVehiculo(T  vehiculo ) {
+        Iterator<T> iterator = lista.iterator();
+        while(iterator.hasNext()){
+            if(iterator.next().equals(vehiculo)){
+                iterator.remove();
+            }
+        }
+    }
+    public void agregarVehiculo(T vehiculo) {
+        lista.add(vehiculo);
+
+    }
+
+    public void listarMotos(){
+        for(T vehiculo : lista){
+            if(vehiculo instanceof Moto){
+                System.out.println("Moto" + vehiculo);
+            }
+        }
+    }
+    public void listarAutos(){
+        for(T vehiculo : lista){
+            if(vehiculo instanceof Auto){
+                System.out.println("Auto" + vehiculo);
+            }
+        }
+    }
+
+    public void listarCamioneta(){
+        for(T vehiculo : lista){
+            if(vehiculo instanceof Camioneta){
+                System.out.println("Camioneta" + vehiculo);
+            }
+        }
+    }
+
 
 }
