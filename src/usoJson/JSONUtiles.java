0e9ee73ba8cded3015b907c4b9ddc5221375e7d4
gregorio4426 <gregoriofacundo4@gmail.com>
2025-11-03 17:36:40 -0300

    libreria json

--- src/usoJson/JSONUtiles.java
+++ src/usoJson/JSONUtiles.java
@@ -1,4 +1,36 @@
 package usoJson;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONTokener;
+
 public class JSONUtiles {
+
+    public static void grabar(JSONArray array, String nombre) {
+        try {
+            FileWriter file = new FileWriter(nombre);
+            file.write(array.toString());
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+
+    public static JSONTokener leer(String archivo) {
+        JSONTokener tokener = null;
+
+        try {
+            tokener = new JSONTokener(new FileReader(archivo));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return tokener;
+    }
 }
